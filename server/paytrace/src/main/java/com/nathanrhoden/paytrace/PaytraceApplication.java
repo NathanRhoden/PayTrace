12d5235fcdda8d1c353af510f35bc797226bd5d9
Nathan <Nathan.rhoden@gmail.com>
2024-09-30 06:56:09 +0100

    Added Springboot server

---
+++ server/paytrace/src/main/java/com/nathanrhoden/paytrace/PaytraceApplication.java
@@ -0,0 +1,13 @@
+package com.nathanrhoden.paytrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaytraceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaytraceApplication.class, args);
+	}
+
+}
---
+++ server/paytrace/src/test/java/com/nathanrhoden/paytrace/PaytraceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathanrhoden.paytrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaytraceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
